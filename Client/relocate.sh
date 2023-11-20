f141c660991b123e053af750357c8cea14760194
ted423 <priston910423@gmail.com>
2023-11-20 15:46:52 +0800

    Update relocate.sh
    注释debug console

--- Client/relocate.sh
+++ Client/relocate.sh
@@ -6,6 +6,6 @@ pwd
 for f in *; do
     url1=$(svn info --show-item repos-root-url "$f")
     url2=$(echo "$url1"|sed 's/192.168.X.X/192.168.X.X/')
-    echo "$url2"
+    #echo "$url2"
     svn relocate "$url1" "$url2" "$f" --username XX --password XX
 done 
